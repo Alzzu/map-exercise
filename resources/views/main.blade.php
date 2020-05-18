a1fef2710342a44259a987849bdacb375e9c6874
Aleksi Vankka <alsu.96@hotmail.fi>
2020-05-18 16:09:52 +0300

    Add retrieving of places and google maps

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -5,8 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title>Map places</title>
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key={{ env('APP_MAPSAPIKEY') }}&callback=initMap" type="text/javascript"></script>
+    <style>
+        #map {
+            height: 30em;
+        }
+    </style>
 </head>
 <body>
     <h1>Places</h1>
+    <div id="map"></div>
+    <ul id="list"></ul>
+
+    <script src="{{ URL::asset('js/main.js')}}"></script>
+    <script src="{{ URL::asset('js/maps.js')}}"></script>
 </body>
 </html>
